9ac3f9442c653710f6a5d9d265172385b02014c8
Pranav-Gangadharan <pranavgangadharan@yahoo.com>
2023-09-25 21:10:40 +0530

    setdefault route path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function App() {
 	return (
@@ -9,7 +10,8 @@ function App() {
 			<BrowserRouter>
 				<Sidebar />
 				<Routes>
-					<Route path='/dashboard' element={<Dashboard/>}/>
+					<Route path='*' element={<Navigate to={'/dashboard'} />} />
+					<Route path='/dashboard' element={<Dashboard />} />
 					{/* futher route in development */}
 				</Routes>
 			</BrowserRouter>
